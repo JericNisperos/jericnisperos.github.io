750aeab73f6fd301c49c28214c53abb506f81119
Jeric Nisperos <jericrnisperos@gmail.com>
2019-04-23 11:15:43 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,30 +1,30 @@
 
 
-function getCssValuePrefix()
-{
-    var rtrnVal = '';//default to standard syntax
-    var prefixes = ['-o-', '-ms-', '-moz-', '-webkit-'];
+// function getCssValuePrefix()
+// {
+//     var rtrnVal = '';//default to standard syntax
+//     var prefixes = ['-o-', '-ms-', '-moz-', '-webkit-'];
 
-    // Create a temporary DOM object for testing
-    var dom = document.createElement('div');
+//     // Create a temporary DOM object for testing
+//     var dom = document.createElement('div');
 
-    // for (var i = 0; i < prefixes.length; i++)
-    // {
-    //     // Attempt to set the style
-    //     dom.style.background = prefixes[i] + 'linear-gradient(#000000, #ffffff)';
+//     for (var i = 0; i < prefixes.length; i++)
+//     {
+//         // Attempt to set the style
+//         dom.style.background = prefixes[i] + 'linear-gradient(#000000, #ffffff)';
 
-    //     // Detect if the style was successfully set
-    //     if (dom.style.background)
-    //     {
-    //         rtrnVal = prefixes[i];
-    //     }
-    // }
+//         // Detect if the style was successfully set
+//         if (dom.style.background)
+//         {
+//             rtrnVal = prefixes[i];
+//         }
+//     }
 
-    dom = null;
-    delete dom;
+//     dom = null;
+//     delete dom;
 
-    return rtrnVal;
-}
+//     return rtrnVal;
+// }
 
 
 function homecolor() {
