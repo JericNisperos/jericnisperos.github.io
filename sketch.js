0b93f10a6e7365c7bc556d76afe98f9d2503c5a9
Jeric Nisperos <jericrnisperos@gmail.com>
2019-04-23 11:15:15 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,17 +8,17 @@ function getCssValuePrefix()
     // Create a temporary DOM object for testing
     var dom = document.createElement('div');
 
-    for (var i = 0; i < prefixes.length; i++)
-    {
-        // Attempt to set the style
-        dom.style.background = prefixes[i] + 'linear-gradient(#000000, #ffffff)';
+    // for (var i = 0; i < prefixes.length; i++)
+    // {
+    //     // Attempt to set the style
+    //     dom.style.background = prefixes[i] + 'linear-gradient(#000000, #ffffff)';
 
-        // Detect if the style was successfully set
-        if (dom.style.background)
-        {
-            rtrnVal = prefixes[i];
-        }
-    }
+    //     // Detect if the style was successfully set
+    //     if (dom.style.background)
+    //     {
+    //         rtrnVal = prefixes[i];
+    //     }
+    // }
 
     dom = null;
     delete dom;
